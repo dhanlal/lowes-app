542f76422bc0349a3ea495254bd4d986a6dda625
dhanlal <dhanlal@valuebound.com>
2020-09-23 12:39:21 +0530

    Updates

---
+++ precache-manifest.ccfde55a3591ff48ea1fff0b3658a414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee5b767663c93e82ce68ed3f8d6e6c4",
+    "url": "/dhanlal/lowes-app/index.html"
+  },
+  {
+    "revision": "40be732503fffd392f64",
+    "url": "/dhanlal/lowes-app/static/css/2.5404495b.chunk.css"
+  },
+  {
+    "revision": "03f6971b6c63ecad930b",
+    "url": "/dhanlal/lowes-app/static/css/main.98a0f67a.chunk.css"
+  },
+  {
+    "revision": "40be732503fffd392f64",
+    "url": "/dhanlal/lowes-app/static/js/2.71de70bc.chunk.js"
+  },
+  {
+    "revision": "65e02339f72b05a0a65ebdf311fa1f75",
+    "url": "/dhanlal/lowes-app/static/js/2.71de70bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f6971b6c63ecad930b",
+    "url": "/dhanlal/lowes-app/static/js/main.dda95983.chunk.js"
+  },
+  {
+    "revision": "32d65b31148f2210bd00",
+    "url": "/dhanlal/lowes-app/static/js/runtime-main.9a86392a.js"
+  }
+]);
